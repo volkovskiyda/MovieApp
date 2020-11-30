27237db9ae7efae75844b99fda4e99e549a94af7
volkovskiyda <volkovskiyda@gmail.com>
2020-11-30 03:05:55 +0200

    homework 3: move to recycler view

---
+++ app/src/main/java/com/gmail/volkovskiyda/movieapp/MovieListAdapter.kt
@@ -0,0 +1,44 @@
+package com.gmail.volkovskiyda.movieapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class MovieListAdapter : ListAdapter<Movie, MovieListAdapter.MovieViewHolder>(MovieDiffUtil) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder =
+        MovieViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+        )
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val movie = getItem(position)
+        with(holder) {
+            title.text = movie.title
+            duration.text = movie.duration
+            genre.text = movie.genre
+            rating.text = movie.rating
+            image.setImageResource(movie.image)
+        }
+    }
+
+    class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val title: TextView = itemView.findViewById(R.id.title)
+        val duration: TextView = itemView.findViewById(R.id.duration)
+        val genre: TextView = itemView.findViewById(R.id.genre)
+        val rating: TextView = itemView.findViewById(R.id.rating)
+        val image: ImageView = itemView.findViewById(R.id.image)
+    }
+
+    object MovieDiffUtil : DiffUtil.ItemCallback<Movie>() {
+        override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean =
+            oldItem.title == newItem.title
+
+        override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean =
+            oldItem == newItem
+    }
+}
--- app/src/main/java/com/gmail/volkovskiyda/movieapp/MovieListFragment.kt
+++ app/src/main/java/com/gmail/volkovskiyda/movieapp/MovieListFragment.kt
@@ -1,20 +1,13 @@
 package com.gmail.volkovskiyda.movieapp
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 
 class MovieListFragment : Fragment() {
-
-    private var onMovieClickListener: MovieClickListener? = null
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        onMovieClickListener = context as? MovieClickListener
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,9 +16,26 @@ class MovieListFragment : Fragment() {
     ): View? = inflater.inflate(R.layout.fragment_movie_list, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.findViewById<View>(R.id.avengers_container).setOnClickListener {
-            onMovieClickListener?.onMovieClicked()
-        }
+        val movieListAdapter = MovieListAdapter()
+        view.findViewById<RecyclerView>(R.id.movie_list).adapter = movieListAdapter
+        val avengerActors = listOf(
+            Actor(getString(R.string.actor_robert), R.drawable.actor_robert)
+        )
+        val movies = listOf(
+            Movie(
+                getString(R.string.movie_list_avengers_end_game),
+                getString(R.string.avengers_duration),
+                R.drawable.movie_avengers,
+                getString(R.string.action_adventure_fantasy),
+                getString(R.string.rating_13),
+                getString(R.string.storyline_avengers),
+                avengerActors
+            )
+        )
+        movieListAdapter.submitList(movies)
+//        view.findViewById<View>(R.id.avengers_container).setOnClickListener {
+//            activity?.let { activity -> (activity as MovieClickListener).onMovieClicked() }
+//        }
     }
 
     interface MovieClickListener {
